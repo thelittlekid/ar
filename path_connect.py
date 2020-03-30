406bc1a0843a7fab6f73021c7ea3170b00a1f681
thelittlekid <yifeioffice@gmail.com>
2020-03-30 17:15:44 -0400

    Find connecting path between two samples in the same category
    Implementation of Algorithm 1 in http://openaccess.thecvf.com/content_cvpr_2018/papers/Fawzi_Empirical_Study_of_CVPR_2018_paper.pdf

--- path_connect.py
+++ path_connect.py
@@ -4,26 +4,96 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import os
+import logging
 import numpy as np
 from scipy.special import softmax
+
 from models.base import Linear_base_model, Convolutional_base_model
 from configuration import conf
 
-from art.attacks import FastGradientMethod, ProjectedGradientDescent
+from art.attacks import FastGradientMethod, BasicIterativeMethod, ProjectedGradientDescent
 from art.classifiers import PyTorchClassifier
 from art.utils import load_mnist as art_load_mnist
+from art.utils import compute_success, get_labels_np_array, check_and_transform_label_format
+from art.config import ART_NUMPY_DTYPE
 
 np.random.seed(2020)
+logger = logging.getLogger(__name__)
+
+
+class MinimalPerturbationIterativeMethod(BasicIterativeMethod):
+
+    def generate(self, x, y=None, **kwargs):
+        """
+        Generate adversarial samples and return them in an array.
+
+        :param x: An array with the original inputs.
+        :type x: `np.ndarray`
+        :param y: Target values (class labels) one-hot-encoded of shape `(nb_samples, nb_classes)` or indices of shape
+                  (nb_samples,). Only provide this parameter if you'd like to use true labels when crafting adversarial
+                  samples. Otherwise, model predictions are used as labels to avoid the "label leaking" effect
+                  (explained in this paper: https://arxiv.org/abs/1611.01236). Default is `None`.
+        :type y: `np.ndarray`
+        :return: An array holding the adversarial examples.
+        :rtype: `np.ndarray`
+        """
+        assert x.shape[0] == 1, "x.shape[0] != 1: Minimal perturbation calculation only works for individual sample. "
+        y = check_and_transform_label_format(y, self.classifier.nb_classes())
+
+        if y is None:
+            # Throw error if attack is targeted, but no targets are provided
+            if self.targeted:
+                raise ValueError("Target labels `y` need to be provided for a targeted attack.")
+
+            # Use model predictions as correct outputs
+            targets = get_labels_np_array(self.classifier.predict(x, batch_size=self.batch_size))
+        else:
+            targets = y
+
+        adv_x_best = None
+        rate_best = None
+
+        if self.random_eps:
+            ratio = self.eps_step / self.eps
+            self.eps = np.round(self.norm_dist.rvs(1)[0], 10)
+            self.eps_step = ratio * self.eps
+
+        for _ in range(max(1, self.num_random_init)):
+            adv_x = x.astype(ART_NUMPY_DTYPE)
+
+            for i_max_iter in range(self.max_iter):
+                adv_x = self._compute(
+                    adv_x,
+                    x,
+                    targets,
+                    self.eps,
+                    self.eps_step,
+                    self._project,
+                    self.num_random_init > 0 and i_max_iter == 0,
+                )
+
+                pred = np.argmax(self.classifier.predict(adv_x), axis=1)[0]
+                if pred == np.argmax(targets[0]):
+                    print('Target achieved at iteration %d' % i_max_iter)
+                    break
+
+            if self.num_random_init > 1:
+                rate = 100 * compute_success(
+                    self.classifier, x, targets, adv_x, self.targeted, batch_size=self.batch_size
+                )
+                if rate_best is None or rate > rate_best or adv_x_best is None:
+                    rate_best = rate
+                    adv_x_best = adv_x
+            else:
+                adv_x_best = adv_x
+
+        return adv_x_best
+
 
 try:
     import cPickle as pickle
 except ImportError:  # python 3.x
     import pickle
-
-
-def save_his():
-    with open(filename, 'wb') as outfile:
-        pickle.dump(history, outfile, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 class BCEOneHotLoss(nn.BCELoss):
@@ -36,13 +106,9 @@ def compute_confidence(preds, head='FC'):
 
 
 # Load pretrained model
-HEAD = 'DE'
+HEAD = 'FC'
 FEATURE = 'NN'
-NUM_DISTR = 'num_distr=5'
-
-filename = 'history/{}_{}'.format(HEAD,NUM_DISTR)
-history = {}
-save_his()
+NUM_DISTR = 'num_distr=1'
 
 
 model_directory = os.path.join('ckp', NUM_DISTR, FEATURE)
@@ -66,11 +132,11 @@ else:
     CHECKPOINT = os.path.join(model_directory, 'mnist_FC.pt')
     criterion = nn.CrossEntropyLoss()
 
-model = Convolutional_base_model() if FEATURE == 'CNN' else Linear_base_model()
 
+model = Convolutional_base_model() if FEATURE == 'CNN' else Linear_base_model()
 checkpoint = torch.load(CHECKPOINT)
 model.load_state_dict(checkpoint)
-model.last_layer.training = True
+
 # Step 1: Load the MNIST dataset
 (x_train, y_train), (x_test, y_test), min_pixel_value, max_pixel_value = art_load_mnist()
 # Step 1a: Transpose to N x D format
@@ -95,35 +161,72 @@ classifier = PyTorchClassifier(
     preprocessing=(0.1307, 0.3081)
 )
 
-# Step 5: Evaluate the ART classifier on benign test examples
-predictions = classifier.predict(x_test)
-accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
-print("Accuracy on benign test examples: {}%".format(accuracy * 100))
-confidence = np.mean(compute_confidence(predictions, HEAD))
-print("Average confidence on benign test examples: %f" % confidence)
+# Define attack
+eps = 0.01
+attack = MinimalPerturbationIterativeMethod(classifier=classifier, eps=eps, eps_step=eps/3, targeted=True, max_iter=999)
 
-# FGSM
-history = {}
-for eps in np.linspace(.01, .3, 30):
-    accs, confs = [], []
-    for repeat in range(1):
-        print("eps = %f, run %d" % (eps, repeat))
-        # Step 6: Generate adversarial test examples
-        attack = FastGradientMethod(classifier=classifier, eps=eps, eps_step=eps/3)
-        x_test_adv = attack.generate(x=x_test)
 
-        # Step 7: Evaluate the ART classifier on adversarial test examples
-        predictions = classifier.predict(x_test_adv)
-        accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
-        print("Accuracy on adversarial test examples: {}%".format(accuracy * 100))
-        confidence = np.mean(compute_confidence(predictions, HEAD))
-        print("Average confidence on adversarial test examples: %f" % confidence)
+def get_data_pairs(xs, num=1):
+    data_pairs = []
+    predictions = np.argmax(classifier.predict(xs), axis=1)
+    labels = np.unique(predictions)
+    for label in labels:
+        indices = np.argwhere(predictions == label).squeeze()
+        for i in range(num):
+            indices = np.random.permutation(indices)
+            data_pairs.append((xs[indices[0], ...], xs[indices[-1], ...]))
+    np.random.shuffle(data_pairs)
+    return data_pairs
 
-        accs.append(accuracy)
-        confs.append(confidence)
-        history[eps] = accs
 
-    print("eps = %f, mean accuracy on adversarial test examples: %f ~ %f, mean confidence %f" %
-          (eps, np.mean(accs), np.std(accs), np.mean(confs)))
+def check_path_validity(xstart, xend, label, convex_comb=4):
+    step_vec = (xend - xstart) / (convex_comb + 1)
+    anchors = []
+    for i in range(1, convex_comb + 1):
+        anchors.append(xstart + step_vec * i)
+    anchors = np.stack(anchors)
+    preds = np.argmax(classifier.predict(anchors), axis=1)
+    unique_preds = np.unique(preds)
+    return False if len(unique_preds) != 1 or unique_preds[0] != label else True
 
-save_his()
+
+def find_path(x1, x2, degree=0):
+    if degree > 10:
+        print("No connected path found within 10 recursion")
+        return [x1, x2]
+
+    x = np.stack((x1, x2))
+    preds = np.argmax(classifier.predict(x), axis=1)
+    if len(np.unique(preds)) > 1:
+        print("Input samples belong to different categories according to the classifier")
+        return []
+    else:
+        label = np.unique(preds)[0]
+
+    xm = (x1 + x2) / 2
+    x_ = np.expand_dims(xm, axis=0)
+    pred_m = np.argmax(classifier.predict(x_), axis=1)[0]
+    if pred_m != label:
+        params = {'y': torch.eye(classifier.nb_classes())[[label]].numpy()}
+        x_adv = attack.generate(x_, **params)
+        xm = x_adv.squeeze()
+
+        if np.argmax(classifier.predict(x_adv), axis=1)[0] != label:
+            print("No connected path found because the mid-point cannot be perturbed to the target category")
+            return []
+
+    # Check the validity of the path and keep finding recursively when it's invalid
+    path1 = [x1, xm] if check_path_validity(x1, xm, label) else find_path(x1, xm, degree + 1)
+    path2 = [xm, x2] if check_path_validity(xm, x2, label) else find_path(xm, x2, degree + 1)
+    return path1 + path2[1:]
+
+
+# Step 4: Select data pairs from the same category
+data_pairs = get_data_pairs(x_test, num=1000)
+
+# Step 5: Find a path that connects the two inputs with same predictions
+for idx, data_pair in enumerate(data_pairs):
+    print("Index: %d" % idx)
+    path = find_path(data_pair[0], data_pair[-1])
+    print(len(path))
+pass
